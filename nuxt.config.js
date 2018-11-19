e12088c7355808c9da180199c22fd35e7170af46
Jérôme <jcognet@gmail.com>
2018-11-19 18:21:59 +0100

    Add list beer

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,6 @@ module.exports = {
     env: {},
   modules: [
     'bootstrap-vue/nuxt',
-
-    // Or if you have custom bootstrap CSS...
-    ['bootstrap-vue/nuxt', {css: false}],
+    '@nuxtjs/axios'
   ]
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Hello world !</h1>
+  <div>
+    <h1>Hello world !</h1>
+    <div>
+      <ul>
+        <li v-for="(beer,key) in data">
+          {{beer.nameDisplay}}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<script>
+  import axios from "axios";
+
+  export default {
+    asyncData(context) {
+      return axios.get(`https://sandbox-api.brewerydb.com/v2/beers?key=d54e938eaaa5fde135e5a1e4405a6ea6`)
+        .then((res) => {
+          return {data: res.data.data}
+        })
+    }
+  }
+
+</script>
