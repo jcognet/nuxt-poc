9b96450e169c6a1ed8741a8a03e25b01069bb693
Jérôme <jcognet@gmail.com>
2018-11-22 16:02:37 +0100

    Clean Up & update for windows

--- assets/js/beer.js
+++ assets/js/beer.js
@@ -1,6 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 
 export const getBeers = () => {
-  console.log(process.env.API_BEER_BASE_URL+`beers?key=`+process.env.API_BEER_KEY)
   return  axios.get(process.env.API_BEER_BASE_URL+`beers?key=`+process.env.API_BEER_KEY);
 }
