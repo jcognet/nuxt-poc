415a7f6dbbbe9b84c49b09c65cc271440a7d97d5
Jérôme <jcognet@gmail.com>
2018-11-26 14:54:01 +0100

    feat : add list component -ref #1

---
+++ components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <table v-if="dataList.length > 0" class="table table-striped table-hover">
+    <thead class="thead-dark">
+      <tr>
+        <th>Nom de la bière</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(beer, key) in dataList" :key="key">
+        <td>{{ beer.nameDisplay }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    dataList: {
+      type: Array,
+      default: function() {
+        return undefined
+      }
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
     <h1>Liste des bières :)</h1>
-    <div>
-      <ul>
-        <li v-for="(beer, key) in data" :key="key">{{ beer.nameDisplay }}</li>
-      </ul>
-    </div>
+    <div><list :data-list="data" /></div>
   </div>
 </template>
 
 <script>
 import { getBeers } from "~/assets/js/beer"
+import list from "../components/List"
 
 export default {
+  components: { list },
   asyncData() {
     return getBeers().then(res => {
       return { data: res.data.data }
